e76e4e884cd649f2a6634ede44f102919ff62bd7
Jesse Riley <jesse@rileytech.net>
2024-12-20 09:51:39 -0700

    add in some documentation to the home controller

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -9,15 +9,26 @@ using ContosoUniversity.ViewModels;
 
 namespace ContosoUniversity.Controllers
 {
+    /// <summary>
+    /// Controller for handling home page, about page, and contact page requests.
+    /// </summary>
     public class HomeController : Controller
     {
         private SchoolContext db = new SchoolContext();
 
+        /// <summary>
+        /// Returns the default view for the home page.
+        /// </summary>
+        /// <returns>The home page view.</returns>
         public ActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// Retrieves enrollment statistics and displays them on the about page.
+        /// </summary>
+        /// <returns>The about page view with enrollment data.</returns>
         public ActionResult About()
         {
             string query = "SELECT EnrollmentDate, COUNT(*) AS StudentCount "
@@ -28,6 +39,11 @@ namespace ContosoUniversity.Controllers
 
             return View(data.ToList());
         }
+
+        /// <summary>
+        /// Returns the view for the contact page.
+        /// </summary>
+        /// <returns>The contact page view.</returns>
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
@@ -35,6 +51,10 @@ namespace ContosoUniversity.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Releases unmanaged resources and optionally releases managed resources.
+        /// </summary>
+        /// <param name="disposing">True to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
         protected override void Dispose(bool disposing)
         {
             db.Dispose();
