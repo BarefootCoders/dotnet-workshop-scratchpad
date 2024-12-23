a143d6e8b198ee01acbaaab25429fbdac961947f
Jesse Riley <jesse@rileytech.net>
2024-12-23 14:18:46 -0700

    add in documentation from the home controller

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -9,15 +9,26 @@ using ContosoUniversity.ViewModels;
 
 namespace ContosoUniversity.Controllers
 {
+    /// <summary>
+    /// Controller for managing the home page, about page, and contact page.
+    /// </summary>
     public class HomeController : Controller
     {
         private SchoolContext db = new SchoolContext();
 
+        /// <summary>
+        /// Displays the home page.
+        /// </summary>
+        /// <returns>The home page view.</returns>
         public ActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// Displays the about page with student enrollment statistics.
+        /// </summary>
+        /// <returns>The about page view with enrollment data.</returns>
         public ActionResult About()
         {
             string query = "SELECT EnrollmentDate, COUNT(*) AS StudentCount "
@@ -28,6 +39,11 @@ namespace ContosoUniversity.Controllers
 
             return View(data.ToList());
         }
+
+        /// <summary>
+        /// Displays the contact page.
+        /// </summary>
+        /// <returns>The contact page view.</returns>
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
@@ -35,6 +51,10 @@ namespace ContosoUniversity.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Disposes of the database context when the controller is disposed.
+        /// </summary>
+        /// <param name="disposing">True if disposing managed resources.</param>
         protected override void Dispose(bool disposing)
         {
             db.Dispose();
