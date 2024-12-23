aea9a765694ae3fbcfecbd8e66fe79cc031126b0
Jesse Riley <jesse@rileytech.net>
2024-12-23 16:47:28 -0700

    add in options for dependency injection

--- ContosoUniversity.Core.Mvc/Data/SchoolContext.cs
+++ ContosoUniversity.Core.Mvc/Data/SchoolContext.cs
@@ -5,6 +5,11 @@ namespace ContosoUniversity.DAL
 {
     public class SchoolContext : DbContext
     {
+        public SchoolContext(DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
         public DbSet<Course> Courses { get; set; }
         public DbSet<Department> Departments { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
